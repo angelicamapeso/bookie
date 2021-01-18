a1a2728e0e08bcb812044c3b12cd8dfdec880084
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-18 17:34:47 -0500

    Create routes for two pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <h1>Hello world!</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <h1>Home</h1>
+        </Route>
+        <Route exact path="/recommended">
+          <h1>Recommended</h1>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
