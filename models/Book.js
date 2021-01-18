fd0707a2491845a44a0a1658b6f08248ea577ffd
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-18 17:13:23 -0500

    Add booksID field

--- models/Book.js
+++ models/Book.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const bookSchema = new Schema({
+  booksId: {
+    type: String,
+    required: true,
+  },
   title: {
     type: String,
     required: true,
