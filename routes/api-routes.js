f9cf73edf610769a0d2efacc499c7c1631537f6e
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-18 17:23:46 -0500

    Add delete route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const mongoose = require("mongoose");
 const { Book } = require("../models");
 
 router.get("/books", async (req, res) => {
@@ -20,6 +21,19 @@ router.post("/books", async (req, res) => {
       { upsert: true }
     );
     res.status(201).json({ data: result });
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+router.delete("/books/:id", async (req, res) => {
+  try {
+    const _id = mongoose.Types.ObjectId(req.params.id);
+    const result = await Book.deleteOne({ _id });
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ err: "Book not found in database." });
+    }
+    res.status(200).json({ data: result });
   } catch (err) {
     res.status(500).json({ err: err.message });
   }
