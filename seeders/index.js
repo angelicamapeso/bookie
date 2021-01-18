dda881143628650ea561234e3e0395bc120048b8
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-18 17:13:35 -0500

    Update seeders to include booksId field

--- seeders/index.js
+++ seeders/index.js
@@ -3,6 +3,7 @@ const { Book } = require("../models");
 
 const books = [
   {
+    booksId: "fudhwue2",
     title: "Test 1",
     authors: ["Sample Name", "Sample Name2"],
     description: "A test book",
@@ -10,6 +11,7 @@ const books = [
     link: "https://www.google.com",
   },
   {
+    booksId: "frewrew",
     title: "Test 2",
     authors: ["Sample Name"],
     description: "A test book",
@@ -17,6 +19,7 @@ const books = [
     link: "https://www.google.com",
   },
   {
+    booksId: "6437hreew",
     title: "Test 3",
     authors: ["Sample Name", "Sample Name3"],
     description: "A test book",
