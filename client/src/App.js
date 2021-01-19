7929d1108f9157d349f2aeb6bdd1ca89491a4f45
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-18 21:16:51 -0500

    Link Search page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Search from "./pages/Search";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
-          <h1>Home</h1>
-        </Route>
+        <Route exact path="/" component={Search} />
         <Route exact path="/recommended">
           <h1>Recommended</h1>
         </Route>
