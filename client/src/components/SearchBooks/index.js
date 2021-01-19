d5ec173b896f58045d21a3d786e5bd87681fd295
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-18 22:18:48 -0500

    Add logic to search

--- client/src/components/SearchBooks/index.js
+++ client/src/components/SearchBooks/index.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
+import { searchBooks } from "../../utils/API";
 
 function SearchBooks() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [err, setErr] = useState("");
+  const searchInput = useRef();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const toSearch = searchInput.current.value.trim();
+    if (!toSearch) {
+      return setErr("Can't submit an empty book title.");
+    }
+
+    searchBooks(toSearch).then(results => {
+      setErr("");
+      console.log(results);
+      setSearchResults(results);
+    });
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group controlId="search">
         <Form.Label>Search</Form.Label>
         <InputGroup>
-          <Form.Control type="text" placeholder="Book title" />
+          <Form.Control
+            type="text"
+            placeholder="Book title"
+            ref={searchInput}
+          />
           <InputGroup.Append>
             <Button variant="primary" type="submit">
               Search book title
@@ -17,6 +41,7 @@ function SearchBooks() {
           </InputGroup.Append>
         </InputGroup>
       </Form.Group>
+      {err ? <Alert variant="warning">{err}</Alert> : <></>}
     </Form>
   );
 }
