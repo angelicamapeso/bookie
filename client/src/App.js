a5d3d2776c6ea0611ac9594962138b3362967d00
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-18 21:40:34 -0500

    Use bookProvider

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BookProvider } from "./utils/BookContext";
 import Search from "./pages/Search";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Search} />
-        <Route exact path="/recommended">
-          <h1>Recommended</h1>
-        </Route>
-      </Switch>
-    </Router>
+    <BookProvider>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Search} />
+          <Route exact path="/recommended">
+            <h1>Recommended</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </BookProvider>
   );
 }
 
