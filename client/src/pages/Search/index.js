01dca713548be64d2258e28fec547a7854fc0dea
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-18 22:10:20 -0500

    Use SearchBooks component

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -3,6 +3,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Jumbotron from "react-bootstrap/Jumbotron";
+import SearchBooks from "../../components/SearchBooks";
 
 function Search() {
   return (
@@ -13,6 +14,7 @@ function Search() {
             <h1>Search</h1>
             <p>Search for and save a book to the recommended list!</p>
           </Jumbotron>
+          <SearchBooks />
         </Col>
       </Row>
     </Container>
