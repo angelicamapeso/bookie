188a55d6c86b368978940b0801f58826e0979fe5
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-18 22:02:20 -0500

    Add func to search for books

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,3 +1,23 @@
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+export function searchBooks(search) {
+  return fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=${search}&key=${API_KEY}`
+  )
+    .then(response => response.json())
+    .then(result =>
+      result.items.map(item => ({
+        id: item.id,
+        title: item.volumeInfo.title,
+        authors: item.volumeInfo.authors,
+        description: item.volumeInfo.description,
+        image: item.volumeInfo.imageLinks.thumbnail,
+        link: item.volumeInfo.infoLink,
+        saved: false,
+      }))
+    );
+}
+
 export function getSavedBooks() {
   return fetch("/api/books")
     .then(response => response.json())
