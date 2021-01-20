018daefd1b406d5b98bf76b703286d4fa207820f
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-20 16:53:32 -0500

    Fix buttons colliding with title text

--- client/src/components/BookCard/index.js
+++ client/src/components/BookCard/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { useBooks } from "../../utils/BookContext";
@@ -21,12 +23,15 @@ function BookCard({
           link,
         })
       }
+      className="save-button"
     >
       Save
     </Button>
   );
   const deleteButton = (
-    <Button onClick={() => handleDelete(booksId)}>Delete</Button>
+    <Button onClick={() => handleDelete(booksId)} className="delete-button">
+      Delete
+    </Button>
   );
 
   return (
@@ -39,18 +44,21 @@ function BookCard({
         />
       </div>
       <Card.Body>
-        <div className="d-flex flex-row align-items-start mb-3">
-          <div>
+        <Row className="justify-content-between mb-3">
+          <Col>
             <Card.Title>{title}</Card.Title>
             <Card.Text>{authors}</Card.Text>
-          </div>
-          <div className="ml-auto">
-            <Card.Link href={link} className="mr-3">
+          </Col>
+          <Col
+            xs={4}
+            className="d-flex flex-sm-column flex-md-row align-items-end align-items-md-start justify-content-md-end"
+          >
+            <Card.Link href={link} className="mr-md-3 mr-3 mb-2">
               View
             </Card.Link>
             {saved ? deleteButton : saveButton}
-          </div>
-        </div>
+          </Col>
+        </Row>
         <Card.Text>{description}</Card.Text>
       </Card.Body>
     </Card>
